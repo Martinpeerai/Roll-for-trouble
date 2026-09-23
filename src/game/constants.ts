bf4fd148ff2e7f92460c80c27902c007e0cc1f7a
Martinpeerai <boloody.arms@gmail.com>
2026-09-23 09:12:43 +0200

    feat: add core run constants

---
+++ src/game/constants.ts
@@ -0,0 +1,10 @@
+export const GAME_WIDTH = 1280;
+export const GAME_HEIGHT = 720;
+export const WAVE_DURATION_SECONDS = 40;
+export const TOTAL_WAVES = 20;
+
+export enum RunPhase {
+  Combat = 'COMBAT',
+  WaveBoss = 'WAVE_BOSS',
+  Reward = 'REWARD',
+}
